c50dd6f50e2e993d2aab2769929620a14a5e3075
Chiboy <chiexclusive1151@gmail.com>
2022-03-11 04:51:44 +0100

    made it better

--- app/index.js
+++ app/index.js
@@ -3,8 +3,10 @@
 // DEPENDENCIES
 const server = require("./app");
 const PORT = process.env.PORT || 8080;
+const HOST = process.env.HOST || "127.0.0.1"
 
 // HANDLE SERVER INITIALIZATION
-server.bootstrap(async () => server.fastify.listen(PORT))
+server.bootstrap(async () => server.fastify.listen(PORT, HOST))
 
 
+ 
